02806c38bd5e39d510d769e47d8dc015ed8c4572
ThalusA <15985204+ThalusA@users.noreply.github.com>
2019-02-11 17:31:13 +0100

    BoundingBox Reconizion Algorithme

--- recon.py
+++ recon.py
@@ -1,27 +1,13 @@
 import cv2
 import numpy as np
 
-if __name__ == "__main__":
-    large = cv2.imread('input.png', cv2.IMREAD_UNCHANGED)
-    height, width, channels = large.shape
-    rgb = cv2.pyrDown(large)
-    mod_height, mod_width, mod_channels = rgb.shape
-    height, width  = height/mod_height, width/mod_width
-    small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
-    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
-    connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
-    contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    mask = np.zeros(bw.shape, dtype=np.uint8)
-    i = 0
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
-        mask[y:y+h, x:x+w] = 0
-        cv2.drawContours(mask, contours, i, (255, 255, 255), -1)
-        r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
-        if r > 0.45 and w > 8 and h > 8:
-            cv2.rectangle(large, (int(x*width), int(y*height)), (int((x+w-1)*width), int((y+h-1)*height)), (0, 255, 0), 2)
-        i += 1
-    cv2.imwrite('output.png', large)
+def main():
+    image = cv2.imread('input.jpg')
+    detector = cv2.text_TextDetectorCNN.create("model.prototxt", "icdar13.caffemodel")
+    Bbox, confidence = detector.detect(image)
+    for i in range(len(Bbox)):
+        if confidence[i] > 0.90:
+            cv2.rectangle(image, (Bbox[i][0], Bbox[i][1]), (Bbox[i][0]+Bbox[i][2],Bbox[i][1]+Bbox[i][3]), (0, 255, 0), 2)
+    cv2.imwrite('output.jpg', image)
+
+main()
