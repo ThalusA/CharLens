52e2112e4a5aca804161fa471eb3c0bcadb21459
ThalusA <alexandre.monier@epitech.eu>
2019-03-22 23:10:45 +0100

    Model weight and application

--- main.py
+++ main.py
@@ -38,11 +38,15 @@ x_test = scaler.fit_transform(x_test)
 x_train = x_train.reshape(x_train.shape[0] ,28, 28, order="A")
 x_test = x_test.reshape(x_test.shape[0] ,28, 28, order="A")
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=[28, 28]))
-model.add(tf.keras.layers.Dense(128, activation="relu"))
-model.add(tf.keras.layers.Dense(81, activation="relu"))
-model.add(tf.keras.layers.Dense(47, activation="softmax"))
+if os.path.exists('model_weights'):
+    model = tf.keras.models.load_model("model_weights", compile=False)
+
+else:
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=[28, 28]))
+    model.add(tf.keras.layers.Dense(128, activation="relu"))
+    model.add(tf.keras.layers.Dense(81, activation="relu"))
+    model.add(tf.keras.layers.Dense(47, activation="softmax"))
 
 model.summary()
 
@@ -52,23 +56,28 @@ model.compile(
 	metrics=["accuracy"]
 )
 
-history = model.fit(x_train, y_train, epochs=10)
+trainBoolean = str(input("Do you want to test the model or train the model (Y: Train Model ; N: Test Model) : ")).upper()
+if trainBoolean == "Y":
+    train_step = input(
+        "Veuillez renseigner le nombre d'étape d'apprentissage que vous voulez éxécuter : ")
+    history = model.fit(x_train, y_train, epochs=int(
+        train_step), use_multiprocessing=True)
+    tf.keras.models.save_model(
+        model,
+        'model_weights',
+        overwrite=True,
+        include_optimizer=True
+    )
+    loss_curve = history.history["loss"]
+    acc_curve = history.history["accuracy"]
 
-tf.keras.models.save_model(
-    model,
-    'model_weights',
-    overwrite=True,
-    include_optimizer=True
-)
+    plt.plot(loss_curve, color="red", label="Loss Curve")
+    plt.title("Loss")
+    plt.show()
 
+    plt.plot(acc_curve, color="green", label="Accuracy Curve")
+    plt.title("Accurac  y")
+    plt.show()
+elif trainBoolean == "N":
+    history = model.evaluate(x_test, y_test, use_multiprocessing=True)
 
-loss_curve = history.history["loss"]
-acc_curve = history.history["accuracy"]
-
-plt.plot(loss_curve)
-plt.title("Loss")
-plt.show()
-
-plt.plot(acc_curve)
-plt.title("Accuracy")
-plt.show()
