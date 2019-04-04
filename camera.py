6e6e3ea4aa60d8dee53f359bf745746674094feb
ThalusA <alexandre.monier@epitech.eu>
2019-04-04 16:20:29 +0200

    Optimizing camera.py

--- camera.py
+++ camera.py
@@ -1,24 +1,12 @@
 from picamera import PiCamera
-from picamera.array import PiRGBArray
 import time
 def capture_img(img_path='/home/pi/image_camera.jpg', res=(1024,768), vflip=False, hflip=False):
-    camera = PiCamera()
-    camera.resolution = res
-    camera.vflip = vflip
-    camera.hflip = hflip
-    rawCapture = PiRGBArray(camera, size=res)
-    stream = camera.capture_continuous(rawCapture,format="rgb", use_video_port=True)
-    frame = None
-    on = True
-    camera.brightness = 50
-    camera.contrast = 60
-    time.sleep(0.5)
+    camera = PiCamera(resolution=res, vflip=vflip, hflip=hflip, constrat=60)
     print('Demarrage de la previsualisation...')
     camera.start_preview()
-    camera.preview_fullscreen = True
     time.sleep(10)
-    print('Capture de l'' image...')
+    print("Capture de l'image...")
     camera.capture(img_path)
-    print('Image sauvegardee !'.format(img_path))
-    camera.close()    
-capture_img()
+    camera.stop_preview()
+    print('Image sauvegardee dans : {}'.format(img_path))
+    camera.close()
